6113c35c2e36384cde013d6d52f463e403ef194d
viniciusreiss <viniciusreis81@hotmail.com>
2019-07-22 16:53:56 -0300

    containers: fix style, add cpf validation on boleto form and credit card, add custom input

--- src/containers/Finish/index.js
+++ src/containers/Finish/index.js
@@ -1,12 +1,86 @@
 import React, { Component } from 'react'
 import Barcode from 'react-barcode'
+import moment from 'moment'
+import { formatReal } from '../../utils/formatters'
+import { Icon, Button } from 'antd'
+import style from './style.module.css'
 
 class Finish extends Component {
-  render () {
-    const { data } = this.props
+
+  renderInternalError = () => {
     return (
-      <div style={{ display: 'flex', flex: 1, height: '100vh' }}>
-       <Barcode value={data.boleto_barcode} />
+      <div className={style.containerError}>
+        <Icon type="close-circle" style={{ fontSize: 42, color: 'red' }} />
+        <h1 style={{ marginLeft: 5 }}>Erro interno, tente novamente mais tarde!</h1>
+      </div>
+    )
+  }
+
+  render () {
+    const { data: { response } } = this.props
+
+    const total = formatReal(response.amount)
+    const formattedTotal = total.replace(/,/gi, ".")
+    const plotValue = formattedTotal / response.installments
+
+    return (
+      <div>
+        <header className={style.header}>
+          <h1 className={style.logo}>BestPrice</h1>
+        </header>
+        {response.errors ? this.renderInternalError() :
+        <div className={style.wrapper}>
+          <div className={style.containerBg}>
+            <div className={style.containerSucess}>
+              <div className={style.wrapperIcon}>
+                <h2>Sucesso!</h2>
+                <Icon type="check-circle" style={{ fontSize: 32, color: '#008322' }} />
+              </div>
+            </div>
+            <h3>Dados do pagamento</h3>
+            <div className={style.containerInfo}>
+              <p className={style.label}>Nome</p>
+              <span>{response.customer.name}</span>
+            </div>
+
+            <div className={style.containerInfo}>
+              <p className={style.label}>CPF</p>
+              {response.customer.documents.map(doc => <p key={doc.number}>{doc.number}</p> )}
+            </div>
+            <div className={style.containerInfo}>
+              <p className={style.label}>{response.boleto_barcode ? 'Linha digitável' : 'Parcelas'}</p>
+              <p>{response.boleto_barcode || `${response.installments}x`}</p>
+            </div>
+
+            {response.boleto_barcode ?
+              <div className={style.containerInfo}>
+                <p className={style.label}>Vencimento</p>
+                <p>{moment(response.boleto_expiration_date).format('DD/MM/YYYY')}</p>
+              </div> :
+
+              <div className={style.containerInfo}>
+                <p className={style.label}>Valor das parcelas</p>
+                <p>R${plotValue.toFixed(2)}</p>
+              </div>
+            }
+
+            <div className={style.containerInfo}>
+              <p className={style.label}>Valor</p>
+              <p>R${formatReal(response.amount)}</p>
+            </div>
+
+            <div className={style.containerBarcode}>
+              {response.boleto_barcode ? <Barcode value={response.boleto_barcode} /> : null }
+            </div>
+          </div>
+            <Button
+              style={{ marginTop: 25 }}
+              onClick={this.props.onGoToStore}
+            >
+              Voltar para loja
+            </Button>
+        </div>
+        }
       </div>
     )
   }
--- src/containers/OrderSummary/index.js
+++ src/containers/OrderSummary/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { formatPrice } from '../../utils/formatters'
 import { Icon } from 'antd'
 import style from './style.module.css'
@@ -11,6 +11,10 @@ class OrderSummary extends Component {
     const total = sumItems.reduce((acc, cur) => acc + cur, 0)
 
     return (
+    <Fragment>
+    <header className={style.header}>
+       <h1 className={style.logo}>BestPrice</h1>
+    </header>
      <div className={style.wrapper}>
        <div className={style.containerBg}>
        <div className={style.containerTitle}>
@@ -39,7 +43,7 @@ class OrderSummary extends Component {
             <div className={style.containerProductInfo}>
               <div>
                 <p className={style.name}>{item.productName}</p>
-                <p className={style.name}>qtd: {item.quantity}</p>
+                <p className={style.name}>quantidade: {item.quantity}</p>
               </div>
               <p>{formatPrice(item.price * item.quantity)}</p>
             </div>
@@ -52,14 +56,18 @@ class OrderSummary extends Component {
          {formatPrice(total)}
        </div>
        </div>
-       <div style={{ backgroundColor: '#eee', flex: 1, padding: 24 }}>
+       <div className={style.containerPayment}>
          <h2>Pagamento</h2>
-         <CheckoutContainer
-          onSubmitBoleto={this.props.onSubmitBoleto}
-          loading={this.props.loading}
-         />
+          { items.cart.length >= 1 ?
+            <CheckoutContainer
+              onSubmitCreditCard={this.props.onSubmitCreditCard}
+              onSubmitBoleto={this.props.onSubmitBoleto}
+              loadingBoleto={this.props.loadingBoleto}
+              loadingCreditCard={this.props.loadingCreditCard}
+            /> : ' Você nāo possui nenhum produto no carrinho :(' }
        </div>
      </div>
+    </Fragment>
     )
   }
 }
